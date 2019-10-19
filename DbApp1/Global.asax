421fed44cac7fb047dd0b0cfe9fa094ba0a39c15
MahendraShinde <MahendraShinde@synergetics-india.com>
2019-10-19 15:23:54 +0530

    Add project files.

---
+++ DbApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DbApp1.MvcApplication" Language="C#" %>
---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿CREATE table customers
+(
+ CustomerId int primary key identity,
+ FirstName varchar(20) not null,
+ LastName varchar(20) not null,
+ PhoneNo varchar(10),
+ EMail varchar(50)
+)
+
+INSERT into customers(FirstName, LastName, PhoneNo, EMail)
+values ('Dharmendra','Deol','6787687688','dharam@xys.com');
+
+
+INSERT into CUstomers(FirstName, LastName, PhoneNo, EMail)
+values ('Hema','Malini','6787687118','hema@xys.com');
